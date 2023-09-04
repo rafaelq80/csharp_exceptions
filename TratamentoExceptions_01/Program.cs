3e99000ffaa90afd8bdf0f259982cf76a0d6943a
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-09-04 15:16:37 -0300

    Aula Exceptions

---
+++ TratamentoExceptions_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace TratamentoExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0;
+            int numero2 = 0;
+
+            Console.WriteLine("Digite o Dividendo: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o Divisor: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            divide(numero1, numero2);
+        }
+
+        public static void divide(int dividendo, int divisor)
+        {
+            Console.WriteLine("Divisão = " + (dividendo / divisor));
+        }
+
+    }
+}
